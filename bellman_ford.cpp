16aad087d5396d263ba239a114190b1a8cb5d4a6
“Zijing <zijingli@usc.edu>
2016-07-09 00:16:53 -0700

    finished lab1

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -1,93 +1,186 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
 #include <sstream>
 #include <cstdlib>
+#include <vector>
+#include <limits.h>
 
 using namespace std;
 
+/* Count the number of vertex in the given graph. */
 int countSize(string str)
 {
 	int count = 0;
 	for(int i = 0; i < str.size(); i++)
 	{
-		if(str[i] == ',') continue;
-		count++;
+		if(str[i] == ',') count++;
 	}
+	count++;
 	return count;
 }
 
+/* Transform a comma-seperate-form string to corresponding integer array */
 void splitStringtoInt(string str, int* edges)
 {
 
 	int index = 0;
-	for(int i = 0; i < str.size(); i++)
+	int num = -1;
+	stringstream ss(str);
+	string tempstr;
+
+	while(getline(ss, tempstr, ','))
 	{
-		if(str[i] == '*')
+		// If it is *, it's weight is -1, meaning no egde.
+		if(tempstr[0] =='*')
 		{
 			edges[index] = -1;
 			index++;
 		}
-		else if(str[i] == ',') 
-		{
-			continue;
-		}
 		else
 		{
-			int k = atoi(&str[i]);
-			edges[index] = k;
+			edges[index] = atoi(tempstr.c_str());
 			index++;
 		}
 	}
+}
+
+// Ouput the Bellman Ford result to a output file
+void outputResult(int vnum, int* distance, int* predecessor, int iteration, string filename)
+{
+	string ofilebname = filename.substr(0, filename.size()-4) + "_output.txt";
+	ofstream ofile(ofilebname.c_str());
+	if(!ofile.is_open())
+	{
+		cerr << "Fail to create output file" << endl;
+		return;
+	}
+
+	ofile << "0";
+	for(int i = 1; i < vnum; i++)
+	{
+		ofile << "," << distance[i];
+	}
+	ofile << endl;
+
+	ofile << "0" << endl;
+	vector<int> sequence;
+	for(int i = 1; i < vnum; i++)
+	{
+		//ofile << i << "<-" << predecessor[i] << endl;
+		sequence.clear();
+		int pred = predecessor[i];
+		while(pred != -1)
+		{
+			sequence.push_back(pred);
+			pred = predecessor[pred];
+		}
+		while(!sequence.empty())
+		{
+			ofile << sequence.back() << "->";
+			sequence.pop_back();
+		}
+		ofile << i << endl;
+	}
+	ofile << "Iteration:" << iteration << endl;
+}
+
+// The implementation of Bellman Ford Algorithm.
+void bellmanFord(int **mgraph, int v, string outputfile)
+{
+	int d[v];
+	int pred[v];
+	bool identical = true;
+	int iter = 0;
+
+	// Initialization.
+	for(int i = 0; i < v; i++)
+	{
+		d[i] = INT_MAX;
+		pred[i] = -1;
+	}
+	d[0] = 0;
+
+	// Step 1: dynamics programming at most v-1 iterations.
+	for(int i = 1; i < v-1; i++)
+	{
+		iter++;
+		// Step 2: run through all edges.
+		for(int j = 0; j < v; j++)
+		{
+			for(int k = 0; k < v; k++)
+			{
+				if(mgraph[j][k] == 0 || mgraph[j][k] == -1) continue;
+				if(d[j] != INT_MAX && d[k] > d[j] + mgraph[j][k])
+				{
+					d[k] = d[j] +mgraph[j][k];
+					pred[k] = j;
+					identical = false;
+				}
+			}
+		}
+		if(identical) break;
+		else identical = true;
+	}
+
+	// Output the result.
+	outputResult(v, d, pred, iter, outputfile);
 }
 
 int main(int argc, char** argv)
 {
 	if(argc != 2) 
 	{
-		cerr << "Argument Error";
+		cerr << "Argument Error" << endl;
 		return 0;
 	}
 
-	int **graph;
-	int num = 0;
 	ifstream infile;
 	infile.open(argv[1]);
-
-	// Read in graph data and initilize the graph
-	string line;
-	getline(infile, line);
-	int v = countSize(line);
-	graph = new int*[v];
-	for(int i = 0; i < v; i++)
+	if(!infile.is_open())
 	{
-		graph[i] = new int[v];
+		cerr << "Fail to open the file" << endl;
+		return 0;
 	}
-	splitStringtoInt(line, graph[num]);
-	num++;
 
+	// Initilize the graph
+	int **graph;
+	int num = 0;
+	int v = 0;
+	string line;
+
+	// Get the first line in the .csv file.
+	if(getline(infile, line))
+	{
+		// Count the number of vertex.
+		v = countSize(line);
+		graph = new int*[v];
+		for(int i = 0; i < v; i++)
+		{
+			graph[i] = new int[v];
+		}
+		// Initialize the graph with the first line of data.
+		splitStringtoInt(line, graph[num]);
+		num++;
+	}
+
+
+	// Initialize the graph with the rest of lines of data.
 	while(getline(infile, line))
 	{
 		splitStringtoInt(line, graph[num]);
 		num++;
 	}
 
-	for(int i = 0; i < v; i++)
-	{
-		for(int j = 0; j < v; j++)
-		{
-			cout << graph[i][j] <<',';
-		}
-		cout << endl;
-	}
+	// Run Bellman Ford Algorithm.
+	bellmanFord(graph, v, string(argv[1]));
 
+	// Release the memeory allocated.
 	for(int i = 0; i < v; i++)
 	{
 		delete [] graph[i];
 	}
 
 	delete [] graph;
-
 }
 
